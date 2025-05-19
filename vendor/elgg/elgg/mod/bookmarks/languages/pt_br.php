b1d9c602dab04364119e2ae94dbbb3fca9680c8e
Thiago Reis <thiagoaurea@gmail.com>
2025-05-18 22:28:51 -0300

    Create pt_br.php

---
+++ vendor/elgg/elgg/mod/bookmarks/languages/pt_br.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+
+	/**
+	 * Menu items and titles 
+	 */
+	'item:object:bookmarks' => 'Favoritos',
+	'collection:object:bookmarks' => 'Favoritos',
+	'collection:object:bookmarks:group' => 'Favoritos do Grupo',
+	'collection:object:bookmarks:all' => "Todos os Favoritos do Site",
+	'collection:object:bookmarks:friends' => "Favoritos dos Amigos",
+	'add:object:bookmarks' => "Adicionar um Favorito",
+	'edit:object:bookmarks' => "Editar Favoritos",
+	'notification:object:bookmarks:create' => "Enviar uma Notificação quando um Favorito for criado",
+
+	'bookmarks:this' => "Adicionar esta página aos Favoritos",
+	'bookmarks:this:group' => "Adicionado aos Favoritos em '%s'",
+	'bookmarks:none' => 'Não há Favoritos',
+
+	'bookmarks:notify:summary' => 'Novo Favorito chamado \'%s\'',
+	'bookmarks:notify:subject' => 'Novo Favorito: \'%s\'',
+
+	'bookmarks:numbertodisplay' => 'Número de Favoritos para exibir',
+
+	'groups:tool:bookmarks' => 'Habilitar Favoritos do grupo',
+	'groups:tool:bookmarks:description' => 'Permitir que os membros compartilhem Favoritos neste grupo.',
+
+	/**
+ 	* Widget and bookmarklet
+ 	*/
+	'widgets:bookmarks:name' => 'Favoritos',
+	'widgets:bookmarks:description' => "Mostrar seus Favoritos recentes.",
+
+	/**
+ 	* Status messages
+ 	*/
+	'bookmarks:save:success' => "Seu item foi adicionado aos Favoritos com sucesso.",
+	'entity:delete:object:bookmarks:success' => "Favorito excluído!",
+
+	/**
+	 * Error messages
+	 */
+);
