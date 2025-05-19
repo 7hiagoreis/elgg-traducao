9407cdd119581822c7e21f9ae32e8761365f5fd3
Thiago Reis <thiagoaurea@gmail.com>
2025-05-18 22:43:31 -0300

    Create pt_br.php

---
+++ vendor/elgg/elgg/mod/likes/languages/pt_br.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+	'annotation:delete:likes:fail' => "Houve um problema ao remover a sua curtida",
+	'annotation:delete:likes:success' => "A sua curtida foi removida com sucesso!",
+
+	'likes:this' => '%s curtiu',
+	'likes:deleted' => 'Sua curtida foi removida',
+	'likes:see' => 'Ver quem curtiu isso',
+	'likes:remove' => 'Descurtir',
+	'likes:notdeleted' => 'Houve um problema ao remover sua curtida',
+	'likes:likes' => 'Você curtiu este item',
+	'likes:failure' => 'Houve um problema ao curtir este item',
+	'likes:alreadyliked' => 'Você já curtiu este item',
+	'likes:notfound' => 'O item que você está tentando curtir não foi encontrado',
+	'likes:likethis' => 'Curtir',
+	'likes:userlikedthis' => '%s curtida',
+	'likes:userslikedthis' => '%s curtidas',
+	'likes:river:annotate' => 'curtiu',
+
+
+	// notifications. yikes. / Notificações. Socorro!
+	'likes:notifications:subject' => '%s curtiu a sua publicação "%s"',
+	'likes:notifications:body' => '%1$s curtiu a sua publicação "%2$s" em %3$s,
+
+
+Veja a publicação original aqui:
+
+%4$s
+
+ou veja o perfil de %1$s aqui:
+
+%5$s',
+);
