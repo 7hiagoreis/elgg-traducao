3d7e39e4d0cdc325e838b2424bbd68d9ca28ead9
Thiago Reis <thiagoaurea@gmail.com>
2025-05-18 22:42:40 -0300

    Create pt_br.php

---
+++ vendor/elgg/elgg/mod/invitefriends/languages/pt_br.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+
+	'friends:invite' => 'Convide os seus Amigos',
+	
+	'invitefriends:introduction' => 'Para convidar amigos para esta rede, insira os endereços de e-mail deles e uma mensagem que será enviada junto com o convite.',
+	'invitefriends:emails' => 'Endereços de E-mail (um por linha)',
+	'invitefriends:message' => 'Mensagem',
+	'invitefriends:subject' => 'Convite para participar do %s',
+
+	'invitefriends:success' => 'Seus Amigos foram convidados.',
+	'invitefriends:invitations_sent' => 'Convites enviados: %s. Os seguintes problemas foram encontrados:',
+	'invitefriends:email_error' => 'Os seguintes endereços de e-mail não são válidos: %s',
+	'invitefriends:already_members' => 'Os seguintes já são Membros: %s',
+	'invitefriends:noemails' => 'Nenhum endereço de e-mail foi inserido.',
+	
+	'invitefriends:message:default' => 'Hi,
+
+I want to invite you to join my network here on %s.',
+
+	'invitefriends:email' => 'You have been invited to join %s by %s.
+
+They included the following message:
+
+%s
+
+To join, click the following link:
+%s
+
+You will automatically add them as a friend when you create your account.',
+	
+);
