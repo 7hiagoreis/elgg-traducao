6cc96f638be5b31f6d9b427024f49c264011337d
Thiago Reis <thiagoaurea@gmail.com>
2025-05-18 22:45:49 -0300

    Create pt_br.php

---
+++ vendor/elgg/elgg/mod/messageboard/languages/pt_br.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Translation file
+ *
+ * Note: don't change the return array to short notation because Transifex can't handle those during `tx push -s`
+ */
+
+return array(
+
+	/**
+	 * Menu items and titles / Itens do Menu e Títulos
+	 */
+
+	'messageboard:board' => "Mural de Recados",
+	'messageboard:none' => "Ainda não há Mensagens neste Mural",
+	'messageboard:num_display' => "Número de Mensagens a Exibir",
+	'messageboard:owner' => "Mural de Recados de %s",
+	'messageboard:owner_history' => "Publicações de %s no mural de %s",
+
+	/**
+	 * Message board widget river / Atualizações do Widget do Mural
+	 */
+	'river:user:messageboard' => "%s publicou no mural de %s",
+
+	/**
+	 * Status messages / Mensagens de Status
+	 */
+
+	'annotation:delete:messageboard:fail' => "Desculpe, não foi possível apagar esta Mensagem.",
+	'annotation:delete:messageboard:success' => "Mensagem apagada com sucesso!",
+	
+	'messageboard:posted' => "Você publicou no Mural com sucesso!",
+
+	/**
+	 * Email messages / Mensagens de E-mail
+	 */
+
+	'messageboard:email:subject' => 'Você recebeu um novo comentário no Mural!',
+	'messageboard:email:body' => "Você recebeu um novo comentário no Mural de %s.
+
+O texto é:
+
+%s
+
+Para ver os comentários do seu Mural, clique aqui:
+%s
+
+Para ver o Perfil de %s, clique aqui:
+%s",
+
+	/**
+	 * Error messages / Mensagens de Erro
+	 */
+
+	'messageboard:blank' => "Desculpe, você precisa escrever algo no Campo da Mensagem antes de salvar.",
+	
+	'messageboard:failure' => "Ocorreu um erro inesperado ao adicionar sua Mensagem. Por favor, tente novamente.",
+
+	'widgets:messageboard:name' => "Mural de Mensagens",
+	'widgets:messageboard:description' => "Este é um Mural que você pode colocar no seu Perfil para que outros Usuários possam comentar.",
+);
